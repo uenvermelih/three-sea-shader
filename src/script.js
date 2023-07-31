560f17fb23ab48bc27a73ecf15b8e6ed31eb2087
uen <77583577+uenvermelih@users.noreply.github.com>
2023-07-31 15:56:13 +0200

    downscale

--- src/script.js
+++ src/script.js
@@ -35,7 +35,7 @@ scene.background = environmentMap
  * Water
  */
 // Geometry
-const waterGeometry = new THREE.PlaneGeometry(100, 100, 2048, 2048)
+const waterGeometry = new THREE.PlaneGeometry(100, 100, 1024, 1024)
 //const waterGeometry = new THREE.SphereGeometry(20,120,120)
 
 
